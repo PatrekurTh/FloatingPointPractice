6d1b1da14aa63a822c43dbed33c4ae0e78d39201
Patrekur <patrekurth@gmail.com>
2021-11-11 09:55:04 +0000

    art update

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import os
 from funcs import *
 from FloatingPointDecode import *
 from FloatingPointEncode import *
+from text_colors import bcolors
 
 def main():
     os.system('cls')
 
     quit = False
-    de_en = input("Do you want to practice encoding or decoding? (e/d) ")
+    print_welcome()
+
+    de_en = input("Do you want to practice encoding or decoding (e/d)? ")
+    print(f"{bcolors.INFO}Higher difficulty only increases the {bcolors.ENDC}", end="")
+    print(f"{bcolors.WARNING}POTENTIAL {bcolors.ENDC}", end="")
+    print(f"{bcolors.INFO}difficulty of cases{bcolors.ENDC}")
+    print(f"{bcolors.INFO}It does not automatically mean harder cases{bcolors.ENDC}")
     diff = int(input("Enter difficulty (1-5): "))
 
     while not quit:
